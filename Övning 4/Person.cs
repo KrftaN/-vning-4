286169ac5eb1075eebe813bb26a65cf95537b737
KraftaN <63770425+KrftaN@users.noreply.github.com>
2024-04-05 22:43:37 +0200

    Add project files.

---
+++ Övning 4/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Övning_4
+{
+    internal class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public double Height { get; set; }
+        public double Weight { get; set; }
+
+        public string FullName
+        {
+            get { return FirstName + " " + LastName; }
+        }
+
+        public bool IsAdult
+        {
+            get { return (DateTime.Today.Year - BirthDate.Year) >= 18; }
+        }
+
+        public double BMI
+        {
+            get { return Weight / (Height * Height); }
+        }
+    }
+}
---
+++ Övning 4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Övning_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.FirstName = "Malte";
+            person.LastName = "Elwhagen";
+            person.BirthDate = new DateTime(2006, 1, 18);
+            person.Height = 1.91; // meter
+            person.Weight = 73; // kilogram
+
+            Console.WriteLine($"Namn: {person.FullName}");
+            Console.WriteLine($"Vuxen: {person.IsAdult}");
+            Console.WriteLine($"BMI: {person.BMI}");
+        }
+    }
+}
